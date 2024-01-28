6bd172e00298a8f674f1f0a0ff66587a325c1b86
cherriedy <huy5adv@gmail.com>
2024-01-29 00:22:21 +0700

    created at 29/1

---
+++ routes/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+
+Route::group(['namespace' => 'Auth'], function () {
+    Route::group(['prefix' => 'login'], function () {
+        Route::get('', [LoginController::class, 'login'])->name('login');
+        Route::post('', [LoginController::class, 'authentication']);
+    });
+
+    Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+    Route::group(['prefix' => 'register'], function () {
+        Route::get('', [RegisterController::class, 'register'])->name('register');
+        Route::post('', [RegisterController::class, 'store']);
+    });
+});
