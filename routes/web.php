5cadb01b5796674dfb3e58dffb6dfa9d1b069e95
cherriedy <huy5adv@gmail.com>
2024-02-01 20:24:48 +0700

    delete user's router

--- routes/web.php
+++ routes/web.php
@@ -1,44 +1,20 @@
 <?php
 
-use App\Http\Controllers\Api\VNPayApiController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\Shared\RoomController;
+use App\Http\Controllers\Api\VNPayApiController;
+use App\Http\Controllers\Api\VnPayIPNController;
+use App\Http\Controllers\Public\SearchRoomController;
 use App\Http\Controllers\Shared\UserProfileController;
+use App\Http\Controllers\Shared\UserRechargeController;
 use App\Http\Controllers\Public\GetRoomByCategoryController;
 use App\Http\Controllers\Public\GetRoomByLocationController;
-use App\Http\Controllers\Public\SearchRoomController;
-use App\Http\Controllers\Shared\UserRechargeController;
 
 /* PUBLIC */
 Route::get('/', function () {
     return view('public.pages.home.index');
 })->name('index');
-
-Route::group(['prefix' => 'rooms', 'as' => 'rooms.', 'middleware' => 'auth'], function () {
-    Route::get('', [RoomController::class, 'index'])->name('index');
-    Route::get('create', [RoomController::class, 'create'])->name('create');
-
-    Route::post('store', [RoomController::class, 'store'])->name('store');
-
-    Route::group(['prefix' => '{room}'], function () {
-        Route::get('edit', [RoomController::class, 'edit'])->name('edit');
-        Route::get('hide', [RoomController::class, 'hide'])->name('hide');
-        Route::get('active', [RoomController::class, 'active'])->name('active');
-
-        Route::put('update', [RoomController::class, 'update'])->name('update');
-    });
-
-    Route::get('{slug}-{room}', [RoomController::class, 'show'])
-        ->where(['slug' => '[a-z-0-9-]+', 'room' => '[0-9]+'])
-        ->name('show');
-});
-
-/* User */
-Route::resource('users', UserProfileController::class)
-    ->only(['show', 'edit', 'update'])
-    ->middleware('auth');
-Route::get('profile', [UserProfileController::class, 'profile'])->name('profile');
 
 /* Image */
 Route::group(['prefix' => 'images', 'as' => 'images.', 'middleware' => 'auth'], function () {
@@ -70,15 +46,21 @@ Route::group(['prefix' => 'nap-tien', 'as' => 'recharge.', 'middleware' => 'auth
     Route::get('', [UserReChargeController::class, 'index'])->name('index');
 
     Route::get('{slug}-{id}', [UserRechargeController::class, 'redirectRecharge'])
-    ->where(['slug' => '[a-z-0-9-]+', 'id' => '[0-9]+'])
-    ->name('redirect-transfer');
+        ->where(['slug' => '[a-z-0-9-]+', 'id' => '[0-9]+'])
+        ->name('redirect-transfer');
 
     Route::get('chuyen-khoan', [UserRechargeController::class, 'tranferIndex'])->name('transfer');
     Route::post('chuyen-khoan', [UserRechargeController::class, 'transferProcess']);
 
-    Route::get('internet-banking', [UserRechargeController::class, 'internetBankingIndex'])->name('internet-banking');
-    Route::post('internet-banking', [UserRechargeController::class, 'internetBankingProcess']);
-    Route::get('internet-banking/vnpay_return.php', VNPayApiController::class)->name('internet-banking.vnpayReturn');
+    Route::group(['prefix' => 'internet-banking'], function () {
+        Route::get('internet-banking', [UserRechargeController::class, 'internetBankingIndex'])->name('internet-banking');
+        Route::post('internet-banking', [UserRechargeController::class, 'internetBankingProcess']);
+
+        Route::group(['as' => 'internet-banking.'], function () {
+            Route::get('vnpay_return.php', VNPayApiController::class)->name('vnpayReturn');
+            Route::get('vnpay_ipn.php', VnPayIPNController::class)->name('vnpayIPN');
+        });
+    });
 
     Route::get('refund', [UserRechargeController::class, 'refundIndex'])->name('refund');
     Route::post('refund', [UserRechargeController::class, 'refundProcess']);
