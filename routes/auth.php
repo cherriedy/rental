838d0a56e9d6db8b62d8e7c32e39f4e62aff1aee
cherriedy <huy5adv@gmail.com>
2024-02-07 17:12:25 +0700

    remove logout from guest, add forget/get new password

--- routes/auth.php
+++ routes/auth.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgetPasswordController;
 
 Route::group(['namespace' => 'Auth', 'middleware' => 'guest'], function () {
     Route::group(['prefix' => 'login'], function () {
@@ -11,10 +12,17 @@ Route::group(['namespace' => 'Auth', 'middleware' => 'guest'], function () {
         Route::post('', [LoginController::class, 'authentication']);
     });
 
-    Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
 
     Route::group(['prefix' => 'register'], function () {
         Route::get('', [RegisterController::class, 'register'])->name('register');
         Route::post('', [RegisterController::class, 'store']);
     });
+
+    Route::get('forget-password', [ForgetPasswordController::class, 'forgetPasswordIndex'])->name('forget-password');
+    Route::post('forget-password', [ForgetPasswordController::class, 'forgetPasswordProcess']);
+
+    Route::get('get-password/{user}/{token}', [ForgetPasswordController::class, 'getPasswordIndex'])->name('get-password');
+    Route::put('get-password/{user}/{token}', [ForgetPasswordController::class, 'getPasswordProcess']);
 });
+
+Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
