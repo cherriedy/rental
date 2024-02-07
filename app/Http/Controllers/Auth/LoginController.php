2157a5921769503ad9d0cb92ac39ad400a6c10ff
cherriedy <huy5adv@gmail.com>
2024-02-07 17:03:07 +0700

    switch to AJAX

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -11,24 +11,14 @@ class LoginController extends Controller
 {
     public function login()
     {
-        // $this->authorize('login');
-
         return view('auth.login');
     }
 
     public function authentication(LoginUserRequest $request)
     {
-        // $this->authorize('login');
-
         $validated = $request->validated();
 
         if (Auth::attempt($validated)) {
-            // if (Auth::user()->isAdmin) {
-            //     return redirect()->route('admins.dashboard');
-            // }
-
-            // return redirect()->route('index')->with('success', 'Đăng nhập thành công!');
-
             $response = [
                 'status_code' => 200,
                 'message' => 'Đăng nhập thành công',
@@ -38,9 +28,5 @@ class LoginController extends Controller
 
             return response()->json($response);
         }
-
-        // return redirect()->route('login')->withErrors([
-        //     'LoggedInError' => 'Email hoặc số điện thoại không kết nối với bất kì tài khoản nào.',
-        // ]);
     }
 }
