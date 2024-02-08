a672473671d3b4a036c8de4039a1c7e48f2b6c4b
cherriedy <huy5adv@gmail.com>
2024-02-08 22:37:51 +0700

    switch to new layout

--- resources/views/public/pages/categories/index.blade.php
+++ resources/views/public/pages/categories/index.blade.php
@@ -3,13 +3,31 @@
 @section('title', 'Tìm kiếm theo danh mục')
 
 @section('content')
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item">Trang chủ</li>
-        <li class="breadcrumb-item active">{{ $category->name }}</li>
-    </ol>
+    {{-- HEADER --}}
+    <div class="container-fluid header bg-white p-0">
+        <div class="row g-0 align-items-center flex-column-reverse flex-md-row">
+            <div class="col-md-6 p-5 mt-lg-5">
+                <h1 class="display-5 animated fadeIn mb-4">{{ $category->name }}</h1>
+                {{-- <small>{{ $category->title }}</small>
+                <small>{{ $category->description }}</small> --}}
 
-    <h3 class="h3" style="font-weight: 600">{{ $category->title }}</h3>
-    <small>{{ $category->description }}</small>
+                <nav aria-label="breadcrumb animated fadeIn">
+                    <ol class="breadcrumb text-uppercase">
+                        <li class="breadcrumb-item"><a href="{{ route('index') }}">Trang chủ</a></li>
+                        <li class="breadcrumb-item"><a href="">Danh mục</a></li>
+                        <li class="breadcrumb-item text-body active" aria-current="page">{{ $category->name }}</li>
+                    </ol>
+                </nav>
+            </div>
+
+            <div class="col-md-6 animated fadeIn">
+                <img class="img-fluid" src="{{ Vite::asset('resources/images/header.jpg') }}" alt="">
+            </div>
+        </div>
+    </div>
+
+    {{-- SEARCH BAR --}}
+    @include('shared.search-bar-2')
 
     @include('shared.room-list')
 @endsection
