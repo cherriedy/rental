bcbd8ad22d8398255cdd2f113e42439e64368a2b
cherriedy <huy5adv@gmail.com>
2024-02-08 21:59:46 +0700

    add new header and searchbar

--- resources/views/users/rooms/index.blade.php
+++ resources/views/users/rooms/index.blade.php
@@ -3,13 +3,29 @@
 @section('title', 'Quản lý đăng bài')
 
 @section('content')
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item active">Home</li>
-    </ol>
+    {{-- HEADER --}}
+    <div class="container-fluid header bg-white p-0">
+        <div class="row g-0 align-items-center flex-column-reverse flex-md-row">
+            <div class="col-md-6 p-5 mt-lg-5">
+                <h1 class="display-5 animated fadeIn mb-4">Đăng tin mới</h1>
 
-    <div>
-        @include('shared.success-message')
+                <nav aria-label="breadcrumb animated fadeIn">
+                    <ol class="breadcrumb text-uppercase">
+                        <li class="breadcrumb-item"><a href="{{ route('profile') }}">{{ Auth::user()->name }}</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('rooms.index') }}">Quản lý bài đăng</a></li>
+                        <li class="breadcrumb-item text-body active" aria-current="page">Đăng bài</li>
+                    </ol>
+                </nav>
+            </div>
+
+            <div class="col-md-6 animated fadeIn">
+                <img class="img-fluid" src="{{ Vite::asset('resources/images/header.jpg') }}" alt="">
+            </div>
+        </div>
     </div>
+
+    {{-- SEARCH BAR --}}
+    @include('shared.search-bar-2')
 
     <table class="table table-bordered">
         <thead>
