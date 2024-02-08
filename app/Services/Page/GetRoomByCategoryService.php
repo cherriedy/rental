788d6b93b550bf28069a9da6c7aa8c921ab7a9b8
cherriedy <huy5adv@gmail.com>
2024-02-08 13:42:54 +0700

    remove request param from getListRoom func

--- app/Services/Page/GetRoomByCategoryService.php
+++ app/Services/Page/GetRoomByCategoryService.php
@@ -7,13 +7,16 @@ use Illuminate\Http\Request;
 use App\Services\Core\RoomService;
 use App\Services\Core\GetRoomService;
 
-class GetRoomByCategoryService {
-    public static function index(Request $request, Category $category) {
-        $rooms = RoomService::getListRoom($request, $params = [
-            'category_id' => $category->id,
-        ]);
+class GetRoomByCategoryService
+{
+    public static function index(Request $request, Category $category)
+    {
+        $rooms = RoomService::getListRoom(
+            $params = array_merge($request->all(), [
+                'category_id' => $category->id,
+            ]),
+        );
 
         return compact('rooms', 'category');
     }
 }
-
