3d311e1cdc8d2ec05a218539798da77901d96826
cherriedy <huy5adv@gmail.com>
2024-02-08 14:40:14 +0700

    remove avatar and phone fields

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -24,7 +24,6 @@ class UpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'avatar' => 'nullable|image|mimes:png,jpg,jpeg|dimensions:min_width=250,min_height=250|max:3000',
             'name' => 'required|string|min:3|max:40',
             'phone' => ['nullable', 'regex:/^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$/'],
             'facebook' => 'nullable',
@@ -34,15 +33,10 @@ class UpdateUserRequest extends FormRequest
     public function messages(): array
     {
         return [
-            'avatar.image' => 'Không được sử dụng tệp tin khác ngoài hình.',
-            'avatar.mimes' => 'Hình phải có định dạng png, jpg hoặc jpeg.',
-            'avatar.demension' => 'Hình phải có kích thức lớn hơn 250x250',
-            'avatar.max' => 'Hình phải có dung lượng nhỏ hơn 3MB',
             'name.required' => 'Tên không được để trống.',
             'name.min' => 'Tên phải lớn hơn 3 kí tự.',
             'name.max' => 'Tên phải bé hơn 40 kí tự.',
             'phone.regex' => 'Số điện thoại không đúng định dạng.',
-            // 'phone.required' => 'Số điện thoại không được để trống.',
             // 'phone.unique' => 'Số điện thoại đã được sử dụng.',
         ];
     }
