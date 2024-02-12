ca83dd726031bbf3c21305b5965323b21183d716
cherriedy <huy5adv@gmail.com>
2024-02-13 02:19:49 +0700

    fillable->guarded

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,21 +14,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'avatar',
-        'facebook',
-        'isAdmin',
-        'phone',
-        'account_balance',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
