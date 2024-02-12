69761f771f21b574f4c263165fa7a1760825bd12
cherriedy <huy5adv@gmail.com>
2024-02-13 02:32:49 +0700

    remove rechargeSet to rechargehistory config

--- app/Models/RechargeHistory.php
+++ app/Models/RechargeHistory.php
@@ -27,36 +27,6 @@ class RechargeHistory extends Model
         self::STATUS_ERROR => 'Lỗi',
     ];
 
-    const rechargeSet = [
-        'method' => [
-            1 => [
-                'id' => 1,
-                'name' => 'Chuyển khoản',
-                'avatar' => 'https://phongtro123.com/images/bank-transfer.png',
-            ],
-
-            2 => [
-                'id' => 2,
-                'name' => 'Tiền mặt',
-                'avatar' => 'https://phongtro123.com/images/cash.svg',
-            ],
-
-            3 => [
-                'id' => 3,
-                'name' => 'Thanh toán bằng Internet Banking',
-                'avatar' => 'https://phongtro123.com/images/payment-method.svg',
-            ],
-        ],
-
-        'priceType' => [
-            1 => 2000,
-            2 => 20000,
-            3 => 30000,
-            4 => 50000,
-            5 => 80000,
-        ],
-    ];
-
     public function getStatus() {
         return Arr::get($this->statusSet, $this->status);
     }
