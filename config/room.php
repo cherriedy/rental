f4d1b5d391cc75874feb74037c90db72e08b5da1
cherriedy <huy5adv@gmail.com>
2024-02-13 05:00:04 +0700

    fisrt

---
+++ config/room.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Room;
+
+return [
+    'status' => [
+        Room::STATUS_ZERO => '??',
+        Room::STATUS_DEFAULT => 'Khởi tạo',
+        Room::STATUS_PAID => 'Đã thanh toán',
+        Room::STATUS_EXPIRED => 'Hết hạn',
+        Room::STATUS_ACTIVE => 'Hoạt động',
+        Room::STATUS_CANCEL => 'Đã huỷ',
+    ],
+
+    'subject' => [
+        Room::GENDER_ALL => 'Tất cả',
+        Room::GENDER_MALE => 'Nam',
+        Room::GENDER_FEMALE => 'Nữ',
+    ],
+
+    'hot_service' => [
+        Room::SERVICE_ZERO => 'Chưa có dịch vụ',
+        Room::SERVICE_DEFAULT => 'Dịch vụ mặc định',
+        Room::SERVICE_H1 => 'Dịch vụ HOT1',
+        Room::SERVICE_H2 => 'Dịch vụ HOT2',
+        Room::SERVICE_H3 => 'Dịch vụ HOT3',
+        Room::SERVICE_SPECIAL => 'Dịch vụ đặc biệt nổi bật',
+    ],
+];
