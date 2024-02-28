95c3958e19c9e8f13a5b2363b5602a6ce3ff0114
cherriedy <huy5adv@gmail.com>
2024-02-28 16:31:44 +0700

    using path of picture of room

--- resources/views/public/pages/home/index.blade.php
+++ resources/views/public/pages/home/index.blade.php
@@ -50,12 +50,12 @@
                         <div class="col-lg-4 col-md-6">
                             <div class="property-item rounded overflow-hidden">
                                 <div class="position-relative overflow-hidden">
-                                    <a href=""><img class="img-fluid"
-                                            src="{{ Vite::asset('resources/images/property-1.jpg') }}" alt=""></a>
+                                    <a href=""><img class="img-fluid" src="{{ asset('images/' . $room->picture) }}"
+                                            alt=""></a>
 
                                     <div
                                         class="bg-primary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
-                                        {{$room->image->count()}} ảnh</div>
+                                        {{ $room->image->count() }} ảnh</div>
 
                                     <div
                                         class="bg-white rounded-top text-primary position-absolute start-0 bottom-0 mx-4 pt-1 px-3">
