b3198abf69c2dbca9d2a5812cc8f67b356b01f9c
cherriedy <huy5adv@gmail.com>
2024-02-28 20:52:46 +0700

    new layout

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.layout')
 
-@section('title', $user->name)
+@section('title', 'Trang cá nhân của ' . $user->name)
 
 @section('content')
     {{-- HEADER --}}
@@ -26,44 +26,97 @@
     {{-- SEARCH BAR --}}
     @include('shared.blank-search-bar')
 
+    {{-- MAIN CONTENT --}}
+    <div class="main-content">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="px-3 pt-4 pb-2">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <div class="d-flex align-items-center">
+                                <img style="width: 50px; height: 50px;" class="me-3 avatar-sm rounded-circle"
+                                    src="{{ asset('images/' . $user->avatar) }}" alt="{{ $user->name }}">
 
-    <div class="col-6">
-        <div class="card">
-            <div class="px-3 pt-4 pb-2">
-                <div class="d-flex align-items-center justify-content-between">
-                    <div class="d-flex align-items-center">
-                        <img style="width: 150px; height: 150px;" class="me-3 avatar-sm rounded-circle"
-                            src="{{ asset('images/' . $user->avatar) }}" alt="{{ $user->name }}">
+                                <div>
+                                    <h3 class="card-title fs-6 mb-0"><a href="/profile">{{ $user->name }}</a></h3>
+                                    <span class="text-muted" style="font-size: 12px;">@~{{ $user->name }}</span>
+                                </div>
 
-                        <div>
-                            <h3 class="card-title mb-0"><a href="/profile">{{ $user->name }}</a></h3>
-                            <span class="fs-6 text-muted">@~{{ $user->name }}</span>
+                            </div>
+
+                            @can('update', $user)
+                                <a href="{{ route('users.settings.edit') }}" class="btn btn-sm btn-primary">Cập nhật</a>
+                            @endcan
                         </div>
 
-                    </div>
+                        <div class="px-2 mt-4">
+                            @can('getAccountBalance', $user)
+                                <h6 class="h6"> Số dư khả dụng : </h6>
+                                <p class="fs-6 fw-light">{{ number_format($user->account_balance, 0, '', '.') }}đ</p>
+                            @endcan
 
-                    <div>
-                        @can('update', $user)
-                            <div class="mt-3">
-                                <a href="{{ route('users.settings.edit') }}" class="btn btn-primary">Edit</a>
+                            <h6 class="h6"> Email : </h6>
+                            <p class="fs-6 fw-light">{{ $user->email }}</p>
+
+                            <h6 class="h6"> Số điện thoại : </h6>
+                            <p class="fs-6 fw-light">{{ $user->phone }}</p>
+
+                            <h6 class="h6"> Facebook : </h6>
+                            <p class="fs-6 fw-light">{{ $user->facebook }}</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-9">
+                <div class="tab-content">
+                    <div class="row g-4">
+                        @foreach ($rooms as $room)
+                            <div class="col-lg-4 col-md-8">
+                                <div class="property-item rounded overflow-hidden">
+                                    <div class="position-relative overflow-hidden">
+                                        {{-- <a href=""><img class="img-fluid"
+                                                src="{{ asset('images/' . $room->picture) }}" alt=""></a> --}}
+
+
+                                        <a href=""><img class="img-fluid"
+                                                src="{{ Vite::asset('resources/images/property-1.jpg') }}"
+                                                alt=""></a>
+
+                                        <div
+                                            class="bg-primary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
+                                            {{ $room->image->count() }} ảnh</div>
+
+                                        <div
+                                            class="bg-white rounded-top text-primary position-absolute start-0 bottom-0 mx-4 pt-1 px-3">
+                                            {{ $room->category->name }}</div>
+                                    </div>
+
+                                    <div class="p-4 pb-0">
+                                        <h5 class="text-primary mb-3">{{ $room->price }}</h5>
+                                        <a class="d-block h5 mb-2"
+                                            style="white-space: nowrap; overflow:hidden; text-overflow: ellipsis;"
+                                            href="{{ route('rooms.show', ['slug' => $room->slug, 'room' => $room->id]) }}">{{ $room->title }}</a>
+                                        <p><i class="fa fa-map-marker-alt text-primary me-2"></i>{{ $room->exact_address }}
+                                        </p>
+                                    </div>
+
+                                    <div class="d-flex border-top">
+                                        <small class="flex-fill text-center border-end py-2"><i
+                                                class="fa fa-ruler-combined text-primary me-2"></i>{{ $room->area }}
+                                            m<sup>2</sup></small>
+                                        <small class="flex-fill text-center border-end py-2"><i
+                                                class="fa-solid fa-venus-mars text-primary me-2"></i>{{ $room->subject_id }}</small>
+                                    </div>
+                                </div>
                             </div>
-                        @endcan
+                        @endforeach
                     </div>
                 </div>
 
-                <div class="px-2 mt-4">
-                    <h5 class="fs-5"> Số dư khả dụng : </h5>
-                    <p class="fs-6 fw-light">{{ number_format($user->account_balance, 0, '', ',') }} vnđ</p>
-
-                    <h5 class="fs-5"> Email : </h5>
-                    <p class="fs-6 fw-light">{{ $user->email }}</p>
-
-                    <h5 class="fs-5"> Phone : </h5>
-                    <p class="fs-6 fw-light">{{ $user->phone }}</p>
-
-                    <h5 class="fs-5"> Facebook : </h5>
-                    <p class="fs-6 fw-light">{{ $user->facebook }}</p>
-
+                <div class="mt-3">
+                    {{ $rooms->links() }}
                 </div>
             </div>
         </div>
